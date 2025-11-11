05dcb9a2268fadcbfa620d21620aa7ed740660e3
kevin-Maca <kmacamonsalve@gmail.com>
2025-11-11 17:33:35 -0500

    Project Creation

---
+++ ProductStore.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProductStore.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
